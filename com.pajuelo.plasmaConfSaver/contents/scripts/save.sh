179953bb1e42a010b04b4fba85425174f1e0510c
BarnabediKartola <51967578+BarnabediKartola@users.noreply.github.com>
2019-06-19 09:54:30 -0300

    Update save.sh

--- com.pajuelo.plasmaConfSaver/contents/scripts/save.sh
+++ com.pajuelo.plasmaConfSaver/contents/scripts/save.sh
@@ -23,6 +23,23 @@ cp $configPath/kwinrulesrc $configFolder/kwinrulesrc
 cp $configPath/lattedockrc $configFolder/lattedockrc
 cp -r $configPath/latte $configFolder/latte
 
+#dolphin config
+cp $configPath/dolphinrc $configFolder/dolphinrc
+#config session desktop
+cp $configPath/ksmserverrc $configFolder/ksmserverrc
+#config input devices
+cp $configPath/kcminputrc $configFolder/kcminputrc
+#shortcuts
+cp $configPath/kglobalshortcutsrc $configFolder/kglobalshortcutsrc
+#klipper config
+cp $configPath/klipperrc $configFolder/klipperrc
+#konsole config
+cp $configPath/konsolerc $configFolder/konsolerc
+#kscreenlocker config
+cp $configPath/kscreenlockerrc $configFolder/kscreenlockerrc
+#krunner config
+cp $configPath/krunnerrc $configFolder/krunnerrc
+
 #plasma themes and widgets
 cp -r $dataPath/plasma $configFolder/plasma
 
@@ -37,6 +54,9 @@ cp -r $dataPath/icons $configFolder/icons
 
 #color-schemes
 cp -r $dataPath/color-schemes $configFolder/color-schemes
+
+#fonts
+cp -r $dataPath/kfontinst $configFolder/kfontinst
 
 #if latte-dock was running when we saved then create a flag file for running it on restore
 if pgrep -x latte-dock > /dev/null

e807651f4b318defff560c1471a93f6c66f5b881
BarnabediKartola <51967578+BarnabediKartola@users.noreply.github.com>
2019-06-19 11:17:16 -0300

    Update load.sh
    add double quote to prevent globbing and word splitting.

--- com.pajuelo.plasmaConfSaver/contents/scripts/load.sh
+++ com.pajuelo.plasmaConfSaver/contents/scripts/load.sh
@@ -5,71 +5,72 @@ dataPath=$3
 modelData=$4
 
 if [ -d "$savePath/$modelData/icons" ]; then
-    mv $dataPath/icons $dataPath/icons.bak
-    cp -r $savePath/$modelData/icons $dataPath    
+    mv "$dataPath/icons" "$dataPath/icons.bak"
+    cp -r "$savePath/$modelData/icons" "$dataPath"    
 fi
 
 if [ -d "$savePath/$modelData/color-schemes" ]; then
-    mv $dataPath/color-schemes $dataPath/color-schemes.bak
-    cp -r $savePath/$modelData/color-schemes $dataPath    
+    mv "$dataPath/color-schemes" "$dataPath/color-schemes.bak"
+    cp -r "$savePath/$modelData/color-schemes" "$dataPath"    
 fi
 
 if [ -d "$savePath/$modelData/plasma" ]; then
-    mv $dataPath/plasma $dataPath/plasma.bak
-    cp -r $savePath/$modelData/plasma $dataPath    
+    mv "$dataPath/plasma" "$dataPath/plasma.bak"
+    cp -r "$savePath/$modelData/plasma" "$dataPath"    
 fi
 
 if [ -d "$savePath/$modelData/wallpapers" ]; then
-    mv $dataPath/wallpapers $dataPath/wallpapers.bak
-    cp -r $savePath/$modelData/wallpapers $dataPath    
+    mv "$dataPath/wallpapers" "$dataPath/wallpapers.bak"
+    cp -r "$savePath/$modelData/wallpapers" "$dataPath"    
 fi
 
 if [ -d "$savePath/$modelData/kfontinst" ]; then
-    mv $dataPath/kfontinst $dataPath/kfontinst.bak
-    cp -r $savePath/$modelData/kfontinst $dataPath    
+    mv "$dataPath/kfontinst" "$dataPath/kfontinst.bak"
+    cp -r "$savePath/$modelData/kfontinst" "$dataPath"    
 fi
 
 #backups
-mv $configPath/plasma-org.kde.plasma.desktop-appletsrc $configPath/plasma-org.kde.plasma.desktop-appletsrc.bak
-mv $configPath/.config/plasmarc $configPath/.config/plasmarc.bak
-mv $configPath/.config/plasmashellrc $configPath/.config/plasmashellrc.bak
+mv "$configPath/plasma-org.kde.plasma.desktop-appletsrc" "$configPath/plasma-org.kde.plasma.desktop-appletsrc.bak"
+mv "$configPath/.config/plasmarc" "$configPath/.config/plasmarc.bak"
+mv "$configPath/.config/plasmashellrc" "$configPath/.config/plasmashellrc.bak"
 
 # plasma config files
-cp $savePath/$modelData/plasma-org.kde.plasma.desktop-appletsrc $configPath/plasma-org.kde.plasma.desktop-appletsrc
-cp $savePath/$modelData/plasmarc $configPath/plasmarc
-cp $savePath/$modelData/plasmashellrc $configPath/plasmashellrc
-cp $savePath/$modelData/kdeglobals $configPath/kdeglobals
+cp "$savePath/$modelData/plasma-org.kde.plasma.desktop-appletsrc" "$configPath/plasma-org.kde.plasma.desktop-appletsrc"
+cp "$savePath/$modelData/plasmarc" "$configPath/plasmarc"
+cp "$savePath/$modelData/plasmashellrc" "$configPath/plasmashellrc"
+cp "$savePath/$modelData/kdeglobals" "$configPath/kdeglobals"
                                     
 #kwin                                    
-cp $savePath/$modelData/kwinrc $configPath/kwinrc
-cp $savePath/$modelData/kwinrulesrc $configPath/kwinrulesrc
+cp "$savePath/$modelData/kwinrc" "$configPath/kwinrc"
+cp "$savePath/$modelData/kwinrulesrc" "$configPath/kwinrulesrc"
                                     
 #latte-dock config files                                    
-mv $configPath/lattedockrc $configPath/lattedockrc.bak
-mv $configPath/latte $configPath/latte.bak
-mv $configPath/autostart $configPath/autostart.bak
-cp $savePath/$modelData/lattedockrc $configPath/lattedockrc 
-cp -r $savePath/$modelData/latte $configPath
-cp -r $savePath/$modelData/autostart $configPath                                    
+mv "$configPath/lattedockrc" "$configPath/lattedockrc.bak"
+mv "$configPath/latte" "$configPath/latte.bak"
+mv "$configPath/autostart" "$configPath/autostart.bak"
+cp "$savePath/$modelData/lattedockrc" "$configPath/lattedockrc"
+cp -r "$savePath/$modelData/latte" "$configPath"
+cp -r "$savePath/$modelData/autostart" "$configPath"
 
 FILE=$savePath/$modelData/latterun
 
 #dolphin config
-cp $savePath/$modelData/dolphinrc $configPath/dolphinrc
+cp "$savePath/$modelData/dolphinrc" "$configPath/dolphinrc"
 #config session desktop
-cp $savePath/$modelData/ksmserverrc $configPath/ksmserverrc
+cp "$savePath/$modelData/ksmserverrc" "$configPath/ksmserverrc"
 #config input devices
-cp $savePath/$modelData/kcminputrc $configPath/kcminputrc
+cp "$savePath/$modelData/kcminputrc" "$configPath/kcminputrc"
 #shortcuts
-cp $savePath/$modelData/kglobalshortcutsrc $configPath/kglobalshortcutsrc
+cp "$savePath/$modelData/kglobalshortcutsrc" "$configPath/kglobalshortcutsrc"
 #klipper config
-cp $savePath/$modelData/klipperrc $configPath/klipperrc
+cp "$savePath/$modelData/klipperrc" "$configPath/klipperrc"
 #konsole config
-cp $savePath/$modelData/konsolerc $configPath/konsolerc
+cp "$savePath/$modelData/konsolerc" "$configPath/konsolerc"
 #kscreenlocker config
-cp $savePath/$modelData/kscreenlockerrc $configPath/kscreenlockerrc
+cp "$savePath/$modelData/kscreenlockerrc" "$configPath/kscreenlockerrc"
 #krunner config
-cp $savePath/$modelData/krunnerrc $configPath/krunnerrc
+cp "$savePath/$modelData/krunnerrc" "$configPath/krunnerrc"
+
 
 sync
 if [ -f "$FILE" ]; then
@@ -79,5 +80,8 @@ else
     killall latte-dock
 fi
 
+
 qdbus org.kde.KWin /KWin reconfigure 
 konsole -e kquitapp5 plasmashell && kstart5 plasmashell --windowclass plasmashell --window Desktop
+                                     
+                            
